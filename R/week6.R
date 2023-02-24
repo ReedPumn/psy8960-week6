74aec416584b7d1c9eb771a393a895ddb00987a0
Reed W Priest <pries153@umn.edu>
2023-02-24 16:58:44 -0600

    Steps 1:18 done.

--- R/week6.R
+++ R/week6.R
@@ -1,8 +1,8 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-library(stringi)
 library(stringr)
+library(stringi)
 
 
 # Data Import
@@ -12,3 +12,7 @@ length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
 # Data Cleaning
+sample(citations_txt, 10)
+
+citations_tbl <- as_tibble(data.frame(line = 1:45492, cite = citations_txt)) %>%
+  mutate(cite = str_remove_all(citations_txt, pattern = "^\"|\'"))
