4f60c4d05c2e78ab7a90a535d41fdd1788619918
Reed W Priest <pries153@umn.edu>
2023-02-24 19:09:26 -0600

    Steps 1:25 done.

--- R/week6.R
+++ R/week6.R
@@ -1,7 +1,7 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-library(stringr)
+library(rebus)
 library(stringi)
 
 
@@ -15,4 +15,13 @@ mean(str_length(citations_txt))
 sample(citations_txt, 10)
 
 citations_tbl <- as_tibble(data.frame(line = 1:45492, cite = citations_txt)) %>%
-  mutate(cite = str_remove_all(citations_txt, pattern = "^\"|\'"))
+  mutate(cite = str_remove_all(citations_txt, pattern = "^\"|\'")) %>%
+  mutate(year = str_extract(citations_txt, pattern = "(\\d{4})")) %>%
+  mutate(page_start = str_match(citations_txt, pattern = "(\\d+)-")[,2]) %>%
+  mutate(perf_ref = str_detect(str_to_lower(citations_txt), pattern = "performance")) %>%
+  mutate(title = str_match(citations_txt, pattern = "\\d{4}\\)\\. (.*)\\. ")[,2]) %>%
+  mutate(first_author = str_match(citations_txt, pattern = "(\\w+), (\\w)")[,1])
+
+length(citations_tbl$first_author)
+
+
